62665a0833a1be50a42c794c9869efccb581a2aa
sstan1020 <sstan1020@gmail.com>
2024-12-27 13:23:51 +0800

    333

--- lib/test.dart
+++ lib/test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//uuuuu
 void main() {
   runApp(const MyApp());
 }
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return MaterialApp(
       title: '简单计数器',
       theme: ThemeData(
@@ -61,5 +63,46 @@ class _HomeScreenState extends State<HomeScreen> {
         child: const Icon(Icons.add),
       ),
     );
+=======
+    return ListView(
+        padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+        children: [
+          const Divider(),
+          const ListTile(
+            title: Text('Item 1'),
+          ),
+          const Divider(),
+          ListTile(
+            title: const Text('Item 2'),
+            subtitle: const Text('Subtitle'),
+            leading: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1501196354995-cbb66c0ecc51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'),
+            ),
+            trailing: const Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.grey,
+            ),
+            isThreeLine: true,
+            dense: true,
+            contentPadding: const EdgeInsets.all(20),
+            selected: true,
+            selectedTileColor: Colors.blue,
+            selectedColor: Colors.white,
+            enabled: true,
+            onLongPress: () {
+              print('Long press');
+            },
+            onTap: () {
+              print('Tap');
+            },
+          ),
+          const Divider(),
+          const ListTile(
+            title: Text('Item 3'),
+          ),
+          const Divider(),
+        ]);
+>>>>>>> Stashed changes
   }
 }
